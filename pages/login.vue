c867a692546f21babe42cf38946a2e9b12539ff9
GiodiCi <diletta@Dilettas-MacBook-Pro.local>
2020-04-25 18:42:35 +0200

    Login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,47 @@
 <template>
     <div class="login">
-        this is login
+        <form @submit.prevent="login">
+            <label>User email</label>
+            <input 
+                type="text" 
+                placeholder="Email"
+                v-model="loginForm.email"
+            >
+        
+            <label>Password</label>
+            <input 
+                type="password" 
+                placeholder="password"
+                v-model="loginForm.password"
+            >
+            <button type="submit">Login</button>
+        </form>
+
+        <div 
+            class="error"
+            v-if="error"
+        >
+            {{ error.message }}
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                loginForm: {
+                    email: '',
+                    password: '',
+                },
+                error: '',
+            };
+        },
+        methods: {
+            login() {
+                console.log('try to log in');  
+            }
+        }
     }
 </script>
 
